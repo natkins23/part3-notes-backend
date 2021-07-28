d1bac56e5c16d9312c3048abee258c704b23ba82
Nathan Watkins <nathancwatkins23@gmail.com>
2021-07-27 19:45:34 -0700

    changedURL

--- mongo.js
+++ mongo.js
@@ -9,7 +9,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0-ostce.mongodb.net/test?retryWrites=true`
+const url = `mongodb+srv://fullstack:${password}>@cluster0.nqtht.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 mongoose.connect(url, {
   useNewUrlParser: true,
